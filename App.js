7649897e0bc38eb3e2b59e690e588c21aad32b34
Sourav Majumder <souravmajumderk99@gmail.com>
2023-02-18 14:01:09 +0600

    learned scrollview

--- App.js
+++ App.js
@@ -1,32 +1,74 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [addedText, setAddedText] = useState("");
+  const [goals, setGoals] = useState([]);
+  const goalsAdd = () => {
+    setGoals((prev) => [...prev, addedText]);
+  };
+  const inputChange = (text) => {
+    setAddedText(text);
+  };
+  console.log(goals);
   return (
     //view used for hold the layout and our component.. we need to wrap our component within view
     //text is just for string value
-    <View style={styles.container}>
-      <View style={styles.btn}>
-        <Button title="Hello" />
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Type goals"
+          style={styles.textInput}
+          onChangeText={inputChange}
+        />
+        <Button title="Add goals" onPress={goalsAdd} />
       </View>
-      <Text style={{ borderWidth: 2, borderColor: "#eee", color: "#000" }}>
-        Hello i am sourav
-      </Text>
+      {/* Scroolview render all the items which is not visible as well. That will cause performance issues */}
+      <View style={styles.goalsStyle}>
+        <ScrollView>
+          {goals.map((goal) => (
+            <Text key={goal} style={styles.goalItem}>
+              {goal}
+            </Text>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: 40,
+    marginBottom: 30,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  btn: {
-    padding: 20,
-    backgroundColor: "#eee",
-    color: "#fff",
-    marginBottom: 35,
+  main: {
+    padding: 40,
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#eee",
+    width: "100%",
+    padding: 10,
+  },
+  goalsStyle: {
+    flex: 3,
+  },
+  goalItem: {
+    backgroundColor: "blue",
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+    color: "white",
   },
 });
